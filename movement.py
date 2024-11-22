7efd62ab1fd3160dcbff8dd9931c5200a8b31623
tangerine238 <stephentang238@gmail.com>
2024-11-22 15:49:25 -0500

    v1

--- movement.py
+++ movement.py
@@ -1,28 +1,93 @@
-
-import RPi.GPIO as GPIO          
+import RPi.GPIO as GPIO
 from time import sleep
 
-in1 = 24
-in2 = 23
-en = 25
-temp1=1
+GPIO.setwarnings(False)
+
+# Right Motor
+in1 = 21
+in2 = 22
+en_a = 23
+# Left Motor
+in3 = 29
+in4 = 28
+en_b = 27
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
+GPIO.setup(en_a,GPIO.OUT)
+
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(en_b,GPIO.OUT)
+
+q=GPIO.PWM(en_a,100)
+p=GPIO.PWM(en_b,100)
+p.start(75)
+q.start(75)
+
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+GPIO.output(in4,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
 
-p.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
+# Wrap main content in a try block so we can  catch the user pressing CTRL-C and run the
+# GPIO cleanup function. This will also prevent the user seeing lots of unnecessary error messages.
+try:
+# Create Infinite loop to read user input
+   while(True):
+      # Get user Input
+      user_input = input()
 
-while(1):
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
-    print("forward")
-         
+      # To see users input
+      # print(user_input)
+
+      if user_input == 'w':
+         GPIO.output(in1,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+
+         GPIO.output(in4,GPIO.HIGH)
+         GPIO.output(in3,GPIO.LOW)
+
+         print("Forward")
+
+      elif user_input == 's':
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+
+         GPIO.output(in4,GPIO.LOW)
+         GPIO.output(in3,GPIO.HIGH)
+         print('Back')
+
+      elif user_input == 'd':
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+
+         GPIO.output(in4,GPIO.LOW)
+         GPIO.output(in3,GPIO.LOW)
+         print('Right')
+
+      elif user_input == 'a':
+         GPIO.output(in1,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+
+         GPIO.output(in4,GPIO.LOW)
+         GPIO.output(in3,GPIO.LOW)
+         print('Left')
+
+      # Press 'c' to exit the script
+      elif user_input == 'c':
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.LOW)
+
+         GPIO.output(in4,GPIO.LOW)
+         GPIO.output(in3,GPIO.LOW)
+         print('Stop')
+
+# If user press CTRL-C
+except KeyboardInterrupt:
+  # Reset GPIO settings
+  GPIO.cleanup()
+  print("GPIO Clean up")
+
