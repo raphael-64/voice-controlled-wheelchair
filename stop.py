ab9ab57c9a11149a20e159df84e3705609929285
tangerine238 <stephentang238@gmail.com>
2024-11-22 16:27:01 -0500

    final

--- stop.py
+++ stop.py
@@ -1,16 +1,16 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)  # Use Broadcom pin-numbering scheme
-
 # Left
-PIN1_A = 22 #In1 A
-PIN2_A = 21 #in2 A
-ENA_A = 23 #PWM motor
+PIN1_A = 29 #In1 A
+PIN2_A = 31 #in2 A
+ENA_A = 33 #PWM motor
 
 # Right
-PIN1_B = 27   # IN3 for Motor B
-PIN2_B = 11   # IN4 for Motor B
-ENA_B = 26   # PWM for Motor B
+PIN1_B = 40   # IN3 for Motor B
+PIN2_B = 38   # IN4 for Motor B
+ENA_B = 32   # PWM for Motor B
+
+GPIO.setmode(GPIO.BOARD)  # Use Broadcom pin-numbering scheme
 
 # Setup Motor A pins
 GPIO.setup(PIN1_A, GPIO.OUT)
@@ -22,15 +22,16 @@ GPIO.setup(PIN1_B, GPIO.OUT)
 GPIO.setup(PIN2_B, GPIO.OUT)
 GPIO.setup(ENA_B, GPIO.OUT)
 
+
 # Initialize PWM on ENA and ENB pins at 100Hz
 pwm_a = GPIO.PWM(ENA_A, 100)  # Motor A PWM
 pwm_b = GPIO.PWM(ENA_B, 100)  # Motor B PWM
+
+pwm_a.start(75)
+pwm_b.start(75)
 
 GPIO.output(PIN1_A, GPIO.LOW)
 GPIO.output(PIN2_A, GPIO.LOW)
     
 GPIO.output(PIN1_B, GPIO.LOW)
 GPIO.output(PIN2_B, GPIO.LOW)
-    
-pwm_a.ChangeDutyCycle(0)
-pwm_b.ChangeDutyCycle(0)
--- voice-control-vosk.py
+++ voice-control-vosk.py
@@ -1,31 +1,7 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
-import RPi.GPIO as GPIO
 import time
 import os
-
-# Left
-PIN1_A = 22 #In1 A
-PIN2_A = 21 #in2 A
-ENA_A = 23 #PWM motor
-
-# Right
-PIN1_B = 27   # IN3 for Motor B
-PIN2_B = 11   # IN4 for Motor B
-ENA_B = 26   # PWM for Motor B
-
-GPIO.setmode(GPIO.BCM)  # Use Broadcom pin-numbering scheme
-
-# Setup Motor A pins
-GPIO.setup(PIN1_A, GPIO.OUT)
-GPIO.setup(PIN2_A, GPIO.OUT)
-GPIO.setup(ENA_A, GPIO.OUT)
-
-# Setup Motor B pins
-GPIO.setup(PIN1_B, GPIO.OUT)
-GPIO.setup(PIN2_B, GPIO.OUT)
-GPIO.setup(ENA_B, GPIO.OUT)
-
 
 model = Model("/Users/raphael/Documents/Coding/SE101/voice-controlled-wheelchair/vosk-model")  #just my laptop, add the right path 
 recognizer = KaldiRecognizer(model, 16000)
@@ -33,14 +9,6 @@ recognizer = KaldiRecognizer(model, 16000)
 audio = pyaudio.PyAudio()
 stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=4096)
 stream.start_stream()
-
-# Initialize PWM on ENA and ENB pins at 100Hz
-pwm_a = GPIO.PWM(ENA_A, 100)  # Motor A PWM
-pwm_b = GPIO.PWM(ENA_B, 100)  # Motor B PWM
-
-# Start PWM with 0% duty cycle (motors stopped)
-pwm_a.start(0)
-pwm_b.start(0)
 
 def go_forward():
     os.system("python forward.py")
@@ -74,7 +42,7 @@ try:
                 stop_motors()
             elif "turn left" in text:
                 turn_left()
-            elif "turn right" in text:
+            elif "turn right"  in text:
                 turn_right()
 except KeyboardInterrupt:
     print("\nProgram terminated by user")
@@ -83,7 +51,4 @@ finally:
     stream.stop_stream()
     stream.close()
     audio.terminate()
-    pwm_a.stop()
-    pwm_b.stop()
-    GPIO.cleanup()
     print("GPIO Cleaned Up")
