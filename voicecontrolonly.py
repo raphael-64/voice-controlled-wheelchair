0b6eb34cb043710d3e4e5b444906e1ea64107651
tangerine238 <stephentang238@gmail.com>
2024-11-25 14:43:49 -0500

    aijsufdghd

--- voicecontrolonly.py
+++ voicecontrolonly.py
@@ -5,7 +5,7 @@ import socket
 import subprocess
 import os
 
-model = Model("/Users/steph/OneDrive/Documents/Coding/SE101/voice-controlled-wheelchair/vosk_env/vosk-model")  #just my laptop, add the right path 
+model = Model("/Users/richardhuang/Documents/GitHub/se101project-voiceControlledWheelchair/vosk_env/vosk-model")  #just my laptop, add the right path 
 recognizer = KaldiRecognizer(model, 16000)
 IPAdd = "192.168.124.29"
 audio = pyaudio.PyAudio()
@@ -14,30 +14,12 @@ stream.start_stream()
 
 
 # Define server address and port
-SERVER_IP = ''  # Replace with Raspberry Pi's IP address
-SERVER_PORT = 1200
+SERVER_IP = '172.20.10.2'  # Replace with Raspberry Pi's IP address
+SERVER_PORT = 65432
 
 # Message to send
-current_state = "stop"
+MESSAGE = 'go'
 
-def update_state(new_state):
-    global current_state
-    if new_state != current_state:
-        if new_state == "go":
-            os.system("python3 ./commands/go.py| nc "+IPAdd+" 1200")
-            print("go")
-        elif new_state == "back":
-            os.system("python3 ./commands/back.py | nc "+IPAdd+" 1200")
-            print("back")
-        elif new_state == "stop":
-            os.system("python3 ./commands/stop.py | nc "+IPAdd+" 1200")
-            print("stop")
-        elif new_state == "turn left":
-            os.system("python3 ./commands/left.py | nc "+IPAdd+" 1200")
-            print("left")
-        elif new_state == "turn right":
-            os.system("python3 ./commands/right.py| nc "+IPAdd+" 1200")
-            print("turn right")
 
 try:
     while True:
@@ -45,18 +27,28 @@ try:
         if recognizer.AcceptWaveform(data):
             result = recognizer.Result()
             text = eval(result)["text"]
+
             if "go" in text:
-                update_state("go")
+                os.system("python3 ./commands/go.py| nc "+IPAdd+" 1200")
+                print("go")
             elif "back" in text:
-                update_state("back")
+                os.system("python3 ./commands/back.py | nc "+IPAdd+" 1200")
+                print("back")
             elif "stop" in text:
-                update_state("stop")
+                os.system("python3 ./commands/stop.py | nc "+IPAdd+" 1200")
+                print("stop")
             elif "turn left" in text:
-                update_state("left")
+                os.system("python3 ./commands/left.py | nc "+IPAdd+" 1200")
+                print("left")
             elif "turn right" in text:
-                update_state("turn right")
-
-
+                os.system("python3 ./commands/right.py| nc "+IPAdd+" 1200")
+                print("turn right")
+            elif "slow down" in text:
+                os.system("python3 ./commands/down.py | nc "+IPAdd+" 1200")
+                print("down")
+            elif "speed up" in text:
+                os.system("python3 ./commands/up.py| nc "+IPAdd+" 1200")
+                print("up")
 except KeyboardInterrupt:
     print("\nProgram terminated by user")
 
