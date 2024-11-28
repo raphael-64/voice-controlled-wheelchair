bad949f7014c51fed66b0e6b433c480000f72246
wqba <wabarurse@gmail.com>
2024-11-28 18:59:10 -0500

    subprocceses

--- toggle.py
+++ toggle.py
@@ -8,16 +8,20 @@ toggle_variable = 0
 GPIO.setmode(GPIO.BOARD)  # Use Broadcom pin-numbering scheme
 GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+
 try:
     while True:
         if GPIO.input(TOUCH_SENSOR_PIN) == GPIO.HIGH:  
             # print("HELLO")
             # toggle_variable = 1 - toggle_variable
             if toggle_variable == 0:
-                print("NOT RUNNING")
+                bash_process = subprocess.Popen(['bash', 'listener.sh'])
+                python_process = subprocess.Popen(['python3', 'reader.py'])
                 toggle_variable = 1
             else: 
-                print("RUNNING")
+                bash_process.terminate()
+                python_process.terminate()
                 toggle_variable = 0
 
             time.sleep(0.5)  
