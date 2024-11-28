c58a90f30df92c4586e8fd2ec44d66d7d66ebd51
wqba <wabarurse@gmail.com>
2024-11-28 18:14:10 -0500

    created toggle.py

---
+++ touch_sensor/toggle.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+TOUCH_SENSOR_PIN = 3
+toggle_variable = 0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#print("Press the touch sensor to toggle the variable.")
+
+try:
+    while True:
+        if GPIO.input(TOUCH_SENSOR_PIN) == GPIO.LOW:  
+            toggle_variable = 1 - toggle_variable
+            #print(f"Variable is now: {toggle_variable}")
+            time.sleep(0.5)  
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    GPIO.cleanup()
