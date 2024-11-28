8fe01d4ddeb7dd30c83ca68a160a948ef3a1c43c
wqba <wabarurse@gmail.com>
2024-11-28 18:53:22 -0500

    update 2

--- toggle.py
+++ toggle.py
@@ -8,18 +8,17 @@ toggle_variable = 0
 GPIO.setmode(GPIO.BOARD)  # Use Broadcom pin-numbering scheme
 GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#print("Press the touch sensor to toggle the variable.")
-
 try:
     while True:
         if GPIO.input(TOUCH_SENSOR_PIN) == GPIO.LOW:  
-            toggle_variable = 1 - toggle_variable
-            if toggle_variable == 0:
-                print("NOT RUNNING")
-                toggle_variable = 1
-            else: 
-                print("RUNNING")
-                toggle_variable = 0
+            print("HELLO")
+            # toggle_variable = 1 - toggle_variable
+            # if toggle_variable == 0:
+            #     print("NOT RUNNING")
+            #     toggle_variable = 1
+            # else: 
+            #     print("RUNNING")
+            #     toggle_variable = 0
 
             time.sleep(0.5)  
 except KeyboardInterrupt:
