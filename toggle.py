28805ebfc132f8988fd40b50ee07a412dddd1f83
wqba <wabarurse@gmail.com>
2024-11-28 20:05:57 -0500

    asdas

--- toggle.py
+++ toggle.py
@@ -5,6 +5,37 @@ import subprocess
 TOUCH_SENSOR_PIN = 11
 LED = 13
 
+# Left
+PIN1_A = 29 #In1 A
+PIN2_A = 31 #in2 A
+ENA_A = 33 #PWM motor
+
+# Right
+PIN1_B = 40   # IN3 for Motor B
+PIN2_B = 38   # IN4 for Motor B
+ENA_B = 32   # PWM for Motor B
+
+GPIO.setmode(GPIO.BOARD)  # Use Broadcom pin-numbering scheme
+
+# Setup Motor A pins
+GPIO.setup(PIN1_A, GPIO.OUT)
+GPIO.setup(PIN2_A, GPIO.OUT)
+GPIO.setup(ENA_A, GPIO.OUT)
+
+# Setup Motor B pins
+GPIO.setup(PIN1_B, GPIO.OUT)
+GPIO.setup(PIN2_B, GPIO.OUT)
+GPIO.setup(ENA_B, GPIO.OUT)
+
+
+# Initialize PWM on ENA and ENB pins at 100Hz
+pwm_a = GPIO.PWM(ENA_A, 100)  # Motor A PWM
+pwm_b = GPIO.PWM(ENA_B, 100)  # Motor B PWM
+
+pwm_a.start(50)
+pwm_b.start(50)
+
+
 toggle_variable = 0
 
 GPIO.setmode(GPIO.BOARD)  # Use Broadcom pin-numbering scheme
@@ -21,9 +52,9 @@ try:
                 print("INITIATING...")
                 time.sleep(0.5)
 
-                file = open("output.txt", "w")  
-                file.write("stop\n")
-                file.close()  
+                # file = open("output.txt", "w")  
+                # file.write("stop\n")
+                # file.close()  
 
                 bash_process = subprocess.Popen(['bash', 'listener.sh'])
                 python_process = subprocess.Popen(['python3', 'reader.py'])
@@ -32,6 +63,12 @@ try:
             else: 
                 print("TERMINATING...")
                 time.sleep(0.5)
+
+                GPIO.output(PIN1_A, GPIO.LOW)
+                GPIO.output(PIN2_A, GPIO.LOW)
+                    
+                GPIO.output(PIN1_B, GPIO.LOW)
+                GPIO.output(PIN2_B, GPIO.LOW)
 
                 file = open("output.txt", "w")  
                 file.write("stop\n")
