ea23d5148bc5df3a0ac2b0656dba2bd4d80da9b0
wqba <wabarurse@gmail.com>
2024-11-28 19:00:20 -0500

    added messages

--- toggle.py
+++ toggle.py
@@ -16,10 +16,14 @@ try:
             # print("HELLO")
             # toggle_variable = 1 - toggle_variable
             if toggle_variable == 0:
+                print("INITIATING...")
+                time.sleep(0.5)
                 bash_process = subprocess.Popen(['bash', 'listener.sh'])
                 python_process = subprocess.Popen(['python3', 'reader.py'])
                 toggle_variable = 1
             else: 
+                print("TERMINATING...")
+                time.sleep(0.5)
                 bash_process.terminate()
                 python_process.terminate()
                 toggle_variable = 0
